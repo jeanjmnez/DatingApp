9b7f37e78a281e2542f0cf62cfccdfd53e9c8765
jeansoft@hotmail.com <jeansoft@hotmail.com>
2021-02-16 11:49:55 -0400

    Adding Client Login Navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { AccountService } from './../_services/account.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}
+  loggedIn: boolean = false;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model)
+      .subscribe(response=>{
+        console.log(response);
+        this.loggedIn =true;
+      },error => {
+        console.log(error);
+      })
+  }
+}
