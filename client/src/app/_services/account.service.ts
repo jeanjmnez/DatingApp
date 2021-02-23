5e953d83051ccbfc63821095d9d6fcac134ed7d4
Jean Carlos Jimenez <jeanjmnez@gmail.com>
2021-02-22 21:36:10 -0400

    module routing completed

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,9 +1,9 @@
-import { User } from './../_models/user';
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +11,18 @@ import { map } from 'rxjs/operators';
 
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
+
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-
   constructor(private http: HttpClient) { }
 
-  login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((response: any) => {
+  login(model: User) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
