957c97204341a87796b831e771db2347edb46b1c
Jean Carlos Jimenez <jeanjmnez@gmail.com>
2021-03-08 12:18:42 -0400

    Extending user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,14 +1,29 @@
+using System;
+using System.Collections.Generic;
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
 
-        public byte[] Password { get; set; }    
-        public byte[] PasswordSalt { get; set; }    
-
-        
-
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
